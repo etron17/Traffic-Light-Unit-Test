e179a80517b8cedfde921729d7b99d789a839fdf
Dojae <kim408@mcmaster.ca>
2021-11-03 18:04:07 -0400

    d

--- A5/assignment5_part_b.py
+++
@@ -1,5 +0,0 @@
-'''
-Name: Dojae Kim
-#Student Number: 400420323
-Part: B
-'''
--- A5/main.py
+++
@@ -1,7 +0,0 @@
-'''
-Name: Dojae Kim
-Student Number: 400420323
-File name: Assignment 5 main page
-'''
-
-print("Hello this is assignment 5")
